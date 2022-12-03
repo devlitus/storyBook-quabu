69321773859fadd0f7016e1a3a8f79e13d695b61
devlitus <developercarles@gmail.com>
2022-12-03 08:31:01 +0100

    correct file path

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { Button } from '../components/Button';
-import './header.css';
+import PropTypes from 'prop-types'
+import { Button } from '../button/Button'
+import './header.css'
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
   <header>
@@ -26,31 +25,33 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
         <h1>Acme</h1>
       </div>
       <div>
-        {user ? (
+        {user
+          ? (
           <>
             <span className="welcome">
               Welcome, <b>{user.name}</b>!
             </span>
             <Button size="small" onClick={onLogout} label="Log out" />
           </>
-        ) : (
+            )
+          : (
           <>
             <Button size="small" onClick={onLogin} label="Log in" />
             <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
           </>
-        )}
+            )}
       </div>
     </div>
   </header>
-);
+)
 
 Header.propTypes = {
   user: PropTypes.shape({}),
   onLogin: PropTypes.func.isRequired,
   onLogout: PropTypes.func.isRequired,
-  onCreateAccount: PropTypes.func.isRequired,
-};
+  onCreateAccount: PropTypes.func.isRequired
+}
 
 Header.defaultProps = {
-  user: null,
-};
+  user: null
+}
