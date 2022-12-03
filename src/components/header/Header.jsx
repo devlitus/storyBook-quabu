7b57a108b91b3e31f5ea7dfa84c170f51f72d38f
devlitus <developercarles@gmail.com>
2022-12-03 08:11:53 +0100

    first commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import { Button } from '../button/Button'
+import { Button } from '../components/Button'
 import './header.css'
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
--- src/stories/Header.stories.jsx
+++ src/stories/Header.stories.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
-
-import { Header } from '../components/header/Header';
+import { Header } from '../components/header/Header'
 
 export default {
   title: 'Components/Header',
   component: Header,
   parameters: {
-    layout: 'fullscreen',
-  },
-};
+    layout: 'fullscreen'
+  }
+}
 
-const Template = (args) => <Header {...args} />;
+const Template = (args) => <Header {...args} />
 
-export const LoggedIn = Template.bind({});
+export const LoggedIn = Template.bind({})
 LoggedIn.args = {
   user: {
-    name: 'Jane Doe',
-  },
-};
+    name: 'Jane Doe'
+  }
+}
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {};
+export const LoggedOut = Template.bind({})
+LoggedOut.args = {}
