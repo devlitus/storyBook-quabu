51b033161e4ee6fb552b3905c5722d83070b04d0
devlitus <developercarles@gmail.com>
2023-02-12 12:07:51 +0100

    refactor

--- src/components/navigator/Nav.jsx
+++ src/components/navigator/Nav.jsx
@@ -1,9 +1,14 @@
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './nav.css'
 
 export const Nav = ({ linkNav }) => {
   return (
     <div className="nav">
       <div className="nav__links">
+        <div className='nav__links-movile'>
+          <button><FontAwesomeIcon icon={faXmark} /></button>
+        </div>
         {linkNav.map((item, index) => (
           <a href="/" key={index} className='nav__links__link'>
             {item}
